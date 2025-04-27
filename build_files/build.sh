8f06f698a9a0b9c136b74f6f0649c0a334d426c0
Rin Koito <mouichidouryuuseigun@gmail.com>
2025-04-27 12:13:57 -0400

    Change cosmic from fedora repo to the copr so it doesn't app the apps from the cosmic spin

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,9 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y @cosmic-desktop-environment 
+dnf5 copr enable -y ryanabx/cosmic-epoch
+dnf5 install -y cosmic-desktop
+dnf5 copr disable -y ryanabx/cosmic-epoch
 
 # Use a COPR Example:
 #
